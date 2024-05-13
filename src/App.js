fb10b4ef68aaf4f6e0510837fdda668fca847161
Muhammad Dzulfiqar Firdaus <mr.glasses02@gmail.com>
2024-05-13 19:36:12 +0700

    add slicing components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Browse from './components/Browse';
+import Arrived from './components/Arrived';
+import Clients from './components/Clients'
+import AsideMenu from './components/AsideMenu';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello people with the spirit of learning!
-    </h1>
+    <>
+    <Header/>
+    <Hero/>
+    <Browse/>
+    <Arrived/>
+    <Clients/>
+    <AsideMenu/>
+    <Footer/>
+    </>
   );
 }
 
