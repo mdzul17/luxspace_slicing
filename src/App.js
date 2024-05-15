7a0986ceec7c1080ed21a6a92b69b5734f707a96
Muhammad Dzulfiqar Firdaus <mr.glasses02@gmail.com>
2024-05-15 12:53:48 +0700

    add profile page

--- src/App.js
+++ src/App.js
@@ -1,74 +1,97 @@
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Browse from './components/Browse';
-import Arrived from './components/Arrived';
-import Clients from './components/Clients'
-import AsideMenu from './components/AsideMenu';
-import Footer from './components/Footer';
-import Offline from './components/Offline';
-import Splash from './pages/splash';
+import React from "react";
+import Splash from "./pages/Splash.js";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Browse from "./components/Browse";
+import Arrived from "./components/Arrived";
+import Clients from "./components/Clients";
+import AsideMenu from "./components/AsideMenu";
+import Footer from "./components/Footer";
+import Offline from "./components/Offline";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Profile from "./pages/Profile";
 
 function App() {
   const [items, setItems] = React.useState([]);
   const [offlineStatus, setOfflineStatus] = React.useState(!navigator.onLine);
-  const [splashStatus, setSplashStatus] = React.useState(true)
+  const [splashStatus, setSplashStatus] = React.useState(true);
 
   function handleOfflineStatus() {
-    setOfflineStatus(!navigator.onLine)
+    setOfflineStatus(!navigator.onLine);
   }
 
   React.useEffect(() => {
-    (async() => {
-      // const response = await fetch('https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc', {
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //     'accept': 'application/json',
-      //     'x-api-key': process.env.REACT_APP_APIKEY
-      //   }
-      // });
+    (async () => {
 
-      const response = JSON.parse("{data: {a: 1}}")
+      try {
+        const response = await fetch('https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc', {
+          headers: {
+            'Content-Type': 'application/json',
+            'accept': 'application/json',
+            'x-api-key': process.env.REACT_APP_APIKEY
+          }
+        });
+        
+              const { nodes } = await response.json();
+        
+              if(nodes) {
+                setItems(nodes);
+              } else{
+                setItems([])
+              }
+      } catch (error) {
+        console.log(error)
+      }
 
-      const {nodes} = await response.json();
-      setItems(nodes);
-
-      const script = document.createElement('script')
-      script.src = '/carousel.js';
+      const script = document.createElement("script");
+      script.src = "/carousel.js";
       script.async = false;
-      document.body.appendChild(script)
-      
+      document.body.appendChild(script);
+
       handleOfflineStatus();
-      window.addEventListener('online', handleOfflineStatus)
-      window.addEventListener('offline', handleOfflineStatus)
+      window.addEventListener("online", handleOfflineStatus);
+      window.addEventListener("offline", handleOfflineStatus);
 
       setTimeout(() => {
-        setSplashStatus(false)
+        setSplashStatus(false);
       }, 1500);
 
-      return function (){
-        window.removeEventListener('online', handleOfflineStatus)
-        window.removeEventListener('offline', handleOfflineStatus)
-      }
+      return function () {
+        window.removeEventListener("online", handleOfflineStatus);
+        window.removeEventListener("offline", handleOfflineStatus);
+      };
     })();
-  }, [offlineStatus])
+  }, [offlineStatus]);
 
   return (
     <>
-    {splashStatus === true ? <Splash/> : (
-      <>
-      {offlineStatus && <Offline/>}
-      <Header/>
-      <Hero/>
-      <Browse/>
-      <Arrived items={items}/>
-      <Clients/>
-      <AsideMenu/>
-      <Footer/>
-      </>
-    )}
+      {splashStatus === true ? (
+        <Splash />
+      ) : (
+        <>
+          {offlineStatus && <Offline />}
+          <Header />
+          <Hero />
+          <Browse />
+          <Arrived items={items} />
+          <Clients />
+          <AsideMenu />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
 
-export default App;
+export default function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <>
+        <Route path="/" exact element={<App/>} />
+        <Route path="/profile" exact element={<Profile/>} />
+        </>
+      </Routes>
+    </Router>
+  );
+}
